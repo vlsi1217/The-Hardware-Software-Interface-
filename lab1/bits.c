5ea30cdadb139ce67282df96ef8ad1db559fb3d9
HonestManXin <HonestManXin@gmail.com>
2013-06-17 20:32:32 +0800

    first edition# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## Author:    HonestManXin <HonestManXin@gmail.com>## On branch master# Changes to be committed:#   (use "git reset HEAD^1 <file>..." to unstage)##	new file:   docs/README.conflicts#	new file:   lab0/.config#	new file:   lab0/arrays#	new file:   lab0/arrays.c#	new file:   lab1/.config#	new file:   lab1/Makefile#	new file:   lab1/README#	new file:   lab1/bits.c#	new file:   lab1/bits.h#	new file:   lab1/btest#	new file:   lab1/btest.c#	new file:   lab1/btest.h#	new file:   lab1/decl.c#	new file:   lab1/dlc#	new file:   lab1/driver.pl#	new file:   lab1/fshow#	new file:   lab1/fshow.c#	new file:   lab1/ishow#	new file:   lab1/ishow.c#	new file:   lab1/pointer.c#	new file:   lab1/pointer.h#	new file:   lab1/ptest#	new file:   lab1/ptest.c#	new file:   lab1/tests.c#	new file:   lab2/.config#	new file:   lab2/Makefile#	new file:   lab2/asemmblycode#	new file:   lab2/bomb#	new file:   lab2/bomb.c#	new file:   lab2/defuser.txt#	new file:   lab2/instructions.pdf#	new file:   lab3/.config#	new file:   lab3/Makefile#	new file:   lab3/USERID#	new file:   lab3/ass.o#	new file:   lab3/ass.s#	new file:   lab3/assembly#	new file:   lab3/bang.txt#	new file:   lab3/bufbomb#	new file:   lab3/bufbomb.c#	new file:   lab3/dynamite.txt#	new file:   lab3/fizz.txt#	new file:   lab3/hacker.txt#	new file:   lab3/instructions.pdf#	new file:   lab3/makecookie#	new file:   lab3/sendstring#	new file:   lab3/smoke.txt#	new file:   lab3/test.o#	new file:   lab3/test.s#	new file:   lab4/.config#	new file:   lab4/Makefile#	new file:   lab4/cache-test#	new file:   lab4/cache-test-skel.c#	new file:   lab4/cache_16c_4a_4b.o#	new file:   lab4/cache_32c_8a_8b.o#	new file:   lab4/cache_64c_2a_16b.o#	new file:   lab4/check.awk#	new file:   lab4/check.sh#	new file:   lab4/instructions.pdf#	new file:   lab4/mystery-cache.h#	new file:   lab4/mystery0.o#	new file:   lab4/mystery1.o#	new file:   lab4/mystery2.o#	new file:   lab4/mystery3.o#	new file:   lab5/.config#	new file:   lab5/.mm.c.swp#	new file:   lab5/Makefile#	new file:   lab5/README#	new file:   lab5/clock.c#	new file:   lab5/clock.h#	new file:   lab5/clock.o#	new file:   lab5/config.h#	new file:   lab5/fcyc.c#	new file:   lab5/fcyc.h#	new file:   lab5/fcyc.o#	new file:   lab5/fsecs.c#	new file:   lab5/fsecs.h#	new file:   lab5/fsecs.o#	new file:   lab5/ftimer.c#	new file:   lab5/ftimer.h#	new file:   lab5/ftimer.o#	new file:   lab5/instructions.pdf#	new file:   lab5/mdriver#	new file:   lab5/mdriver.c#	new file:   lab5/mdriver.o#	new file:   lab5/memlib.c#	new file:   lab5/memlib.h#	new file:   lab5/memlib.o#	new file:   lab5/mm.c#	new file:   lab5/mm.h#	new file:   lab5/mm.o#	new file:   lab5/traces/amptjp-bal.rep#	new file:   lab5/traces/binary-bal.rep#	new file:   lab5/traces/binary2-bal.rep#	new file:   lab5/traces/cccp-bal.rep#	new file:   lab5/traces/coalescing-bal.rep#	new file:   lab5/traces/cp-decl-bal.rep#	new file:   lab5/traces/expr-bal.rep#	new file:   lab5/traces/random-bal.rep#	new file:   lab5/traces/random2-bal.rep#	new file:   lab5/traces/realloc-bal.rep#	new file:   lab5/traces/realloc2-bal.rep#	new file:   lab5/traces/short1-bal.rep#	new file:   lab5/traces/short2-bal.rep#	new file:   tools/submit-hw#	new file:   tools/update-course#

---
+++ lab1/bits.c
@@ -0,0 +1,277 @@
+/* 
+ * CSE 351 HW1 (Data Lab )
+ * 
+ * <Please put your name and userid here>
+ * 
+ * bits.c - Source file with your solutions to the Lab.
+ *          This is the file you will hand in to your instructor.
+ *
+ * WARNING: Do not include the <stdio.h> header; it confuses the dlc
+ * compiler. You can still use printf for debugging without including
+ * <stdio.h>, although you might get a compiler warning. In general,
+ * it's not good practice to ignore compiler warnings, but in this
+ * case it's OK.  
+ */
+
+#if 0
+/*
+ * Instructions to Students:
+ *
+ * STEP 1: Read the following instructions carefully.
+ */
+
+You will provide your solution to this homework by
+editing the collection of functions in this source file.
+
+INTEGER CODING RULES:
+ 
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code 
+  must conform to the following style:
+ 
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+    
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
+
+ 
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting an integer by more
+     than the word size.
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
+
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
+
+NOTES:
+  1. Use the dlc (data lab checker) compiler (described in the handout) to 
+     check the legality of your solutions.
+  2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+     that you are allowed to use for your implementation of the function. 
+     The max operator count is checked by dlc. Note that '=' is not 
+     counted; you may use as many of these as you want without penalty.
+  3. Use the btest test harness to check your functions for correctness.
+  4. The maximum number of ops for each function is given in the
+     header comment for each function. If there are any inconsistencies 
+     between the maximum ops in the writeup and in this file, consider
+     this file the authoritative source.
+
+/*
+ * STEP 2: Modify the following functions according the coding rules.
+ * 
+ *   IMPORTANT. TO AVOID GRADING SURPRISES:
+ *   Use the dlc compiler to check that your solutions conform
+ *   to the coding rules.
+ */
+
+
+#endif
+// Rating: 1
+/* 
+ * bitAnd - x&y using only ~ and | 
+ *   Example: bitAnd(6, 5) = 4
+ *   Legal ops: ~ |
+ *   Max ops: 8
+ *   Rating: 1
+ */
+int bitAnd(int x, int y) {
+  //return 2;
+    return ~((~x)|(~y));
+}
+/* 
+ * bitXor - x^y using only ~ and & 
+ *   Example: bitXor(4, 5) = 1
+ *   Legal ops: ~ &
+ *   Max ops: 14
+ *   Rating: 1
+ */
+int bitXor(int x, int y) {
+  //return 2;
+  int m = ~(x&y);
+  int n = ~(~x&~y);
+  return m&n;
+}
+/* 
+ * thirdBits - return word with every third bit (starting from the LSB) set to 1
+ * and the rest set to 0
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 8
+ *   Rating: 1
+ */
+int thirdBits(void) {
+
+  int a = 73;
+  int b = 36;
+  int a1 = a<<9;
+  int a2 = a<<24;
+  int a3 = b<<16;
+  int sum = a + a1 + a2 + a3;
+  return sum;
+}
+// Rating: 2
+/* 
+ * fitsBits - return 1 if x can be represented as an 
+ *  n-bit, two's complement integer.
+ *   1 <= n <= 32
+ *   Examples: fitsBits(5,3) = 0, fitsBits(-4,3) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 2
+ */
+int fitsBits(int x, int n) {
+    int nminus = n + (~0);
+    int sig = x>>31;
+    return !(((x>>nminus))^sig);
+}
+/* 
+ * sign - return 1 if positive, 0 if zero, and -1 if negative
+ *  Examples: sign(130) = 1
+ *            sign(-23) = -1
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 10
+ *  Rating: 2
+ */
+int sign(int x) {
+  int isValue = !!x;
+  int sig = x>>31;
+  return isValue | sig;
+}
+/* 
+ * getByte - Extract byte n from word x
+ *   Bytes numbered from 0 (LSB) to 3 (MSB)
+ *   Examples: getByte(0x12345678,1) = 0x56
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2
+ */
+int getByte(int x, int n) {
+  int number = n<<3;
+  return (x>>number)&0xff;
+}
+// Rating: 3
+/* 
+ * logicalShift - shift x to the right by n, using a logical shift
+ *   Can assume that 0 <= n <= 31
+ *   Examples: logicalShift(0x87654321,4) = 0x08765432
+ *   Legal ops: ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 3 
+ */
+int logicalShift(int x, int n) {
+  //return 2;
+  int prex = x>>n;
+  int mask = 1<<31;
+  mask = mask>>n;
+  mask = mask<<1;
+  mask = ~mask;
+ return prex & mask;
+}
+/* 
+ * addOK - Determine if can compute x+y without overflow
+ *   Example: addOK(0x80000000,0x80000000) = 0,
+ *            addOK(0x80000000,0x70000000) = 1, 
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 3
+ */
+int addOK(int x, int y) {
+  int sigX = (x>>31)&1;
+  int sigY = (y>>31)&1;
+  int sigSum = ((x+y)>>31)&1;
+  int same_pri_sig = sigX ^ sigY;
+  int same_result_sig = !(sigX ^ sigSum);
+  return same_pri_sig | same_result_sig; 
+}
+// Rating: 4
+/* 
+ * bang - Compute !x without using !
+ *   Examples: bang(3) = 0, bang(0) = 1
+ *   Legal ops: ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 4 
+ */
+int bang(int x) {
+  int notZero,sig,reverse,reverseSig;
+  sig = (x>>31)&1;
+  reverse = (~x)+1;
+  reverseSig = (reverse>>31)&1;
+  notZero = sig |reverseSig;
+  return notZero ^ 1;
+}
+// Extra Credit: Rating: 3
+/* 
+ * conditional - same as x ? y : z 
+ *   Example: conditional(2,4,5) = 4
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+int conditional(int x, int y, int z) {
+  //return 2;
+  int isZero = !x;
+  int preMask = ~isZero + 1;
+  int reverseMask = ~preMask;
+  return (z&preMask)|(y&reverseMask);
+}
+// Extra Credit: Rating: 4
+/*
+ * isPower2 - returns 1 if x is a power of 2, and 0 otherwise
+ *   Examples: isPower2(5) = 0, isPower2(8) = 1, isPower2(0) = 0
+ *   Note that no negative number is a power of 2.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 4
+ */
+int isPower2(int x) {
+ // return 2;
+ int sig = x>>31;
+ int minus1 = ~0;
+ int xminus1 = x+ minus1;
+ sig = ~sig;
+ return (!(xminus1&x))&sig&(!!x);
+}
---
+++ lab3/ass.s
@@ -0,0 +1,4 @@
+pushq $0x401020
+movabs $0x1bcfdd55799b18ff,%rax
+movq %rax,0x602308
+retq
